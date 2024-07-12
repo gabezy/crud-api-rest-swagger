604a8889610214fbbce321903198c1d91e5ae220
gabezy <gabrielmoreira2705@gmail.com>
2024-07-11 21:18:25 -0300

    add BCrypt encoder to password

--- src/main/java/com/gabezy/projects/api/services/UserService.java
+++ src/main/java/com/gabezy/projects/api/services/UserService.java
@@ -5,6 +5,7 @@ import com.gabezy.projects.api.domain.dtos.DetailsUserDTO;
 import com.gabezy.projects.api.domain.entity.User;
 import com.gabezy.projects.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +17,12 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public DetailsUserDTO save(CreateUserDTO dto) {
+        // encoding the password sent by the user in the registration
+        dto.setPassword(bCryptPasswordEncoder.encode(dto.getPassword()));
         User user = new User(dto);
         this.userRepository.save(user);
         return new DetailsUserDTO(user);
@@ -31,6 +37,5 @@ public class UserService {
                 .orElseThrow(() -> new RuntimeException("user not found"));
         return new DetailsUserDTO(user);
     }
-
 
 }
