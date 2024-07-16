f9f6db4a8387f97c24ea369f82cb3ab2b0168d61
gabezy <gabrielmoreira2705@gmail.com>
2024-07-15 22:20:52 -0300

    add token service and token DTO

--- src/main/java/com/gabezy/projects/api/config/security/TokenService.java
+++ src/main/java/com/gabezy/projects/api/config/security/TokenService.java
@@ -3,11 +3,15 @@ package com.gabezy.projects.api.config.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.gabezy.projects.api.domain.dtos.TokenDTO;
 import jakarta.annotation.PostConstruct;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 @Service
@@ -35,12 +39,20 @@ public class TokenService {
 
     // Método para obter o username do token
     public String getSubject(String token) {
+        return decodedJWT(token).getSubject();
+    }
+
+
+    public LocalDateTime getExpireAt(String token) {
+        Instant expireInstant = decodedJWT(token).getExpiresAt().toInstant();
+        return LocalDateTime.ofInstant(expireInstant, ZoneId.of("America/Sao_Paulo"));
+    }
+    private DecodedJWT decodedJWT(String token) {
         try {
             return JWT.require(algorithm)
                     .withIssuer(ISSUER)
                     .build()
-                    .verify(token)
-                    .getSubject();
+                    .verify(token);
         } catch (JWTVerificationException e) {
             throw new RuntimeException("Token JWT inválido " + e.getMessage());
         }
